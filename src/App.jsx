d526491a30aba34c1274aceea28ea10ef1e7e996
眞野初芽 <manohatsume@manohatsumenoMacBook-Pro.local>
2025-11-20 10:22:13 +0900

    インデント修正

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import Detail from "../src/assets/Pages/Detail";
 import Contact from "../src/assets/Pages/Contact";
 
 function App() {
-    return (
-        <>
-        <Home />
-        
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/posts/:id" element={<Detail />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Route>
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <Home />
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<Detail />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App
