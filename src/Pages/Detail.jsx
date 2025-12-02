2d446218a69f6c6f04a43f83a591fb707563d368
眞野初芽 <manohatsume@manohatsumenoMacBook-Pro.local>
2025-12-02 13:57:06 +0900

    課題3の実装

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,10 +1,37 @@
-import { posts } from '../Data/posts'
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 
 export default function PostDetail() {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetcher = async () => {
+      try {
+        const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
+        const data = await res.json()
+        setPosts(data.posts)
+      } catch (err) {
+        console.error("Errro", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetcher()
+  }, [])
+
   const { id } = useParams();
   const post = posts.find(p => p.id === Number(id))
+
+  if (loading) {
+    return (
+      <div className="text-center py-10 text-gray-500 text-lg">
+        読み込み中...
+      </div>
+    );
+  }
 
   if (!post) {
     return <div>記事が見つかりませんでした</div>
