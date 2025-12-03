7e282305a63ce93f3bb0e213733394ca239620ca
v1g4-404 <uiga2222@gmail.com>
2025-12-03 23:38:42 +0900

    Revert "課題3の実装"

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,35 +1,7 @@
 import { Link } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { posts } from '../Data/posts'
 
 export default function PostList() {
-
-  const [posts, setPosts] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const fetcher = async () => {
-      try {
-        const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
-        const data = await res.json()
-        setPosts(data.posts)
-      } catch (err) {
-        console.error("Errro", err);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetcher()
-  }, [])
-
-  if (loading) {
-    return (
-      <div className="text-center py-10 text-gray-500 text-lg">
-        読み込み中...
-      </div>
-    );
-  }
-
   return (
     <div>
       <ul className='block mt-4 mb-4 pl-10'>
